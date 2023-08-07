ee9caacdf28476632229bfa8c83bdba94710ac2b
Rice <riceeechang@gmail.com>
2023-08-07 22:24:23 +0800

    更改start host&port

--- pages/company_archievements/_region.vue
+++ pages/company_archievements/_region.vue
@@ -1,23 +1,23 @@
 <template>
   <section class="page-content">
     <ul class="form-tab">
-      <li class="active">全部</li>
-      <li>北部</li>
-      <li>中部</li>
-      <li>南部</li>
+      <li class="active"><a>全部</a></li>
+      <li><a>北部</a></li>
+      <li><a>中部</a></li>
+      <li><a>南部</a></li>
     </ul>
 
 
     <div class="case-wrap">
-      <div class="case">
-        <div class="thumbnail"><img src></div>
-        <h6>七月沐樂</h6>
+      <div v-for="c in cases" class="case">
+        <div class="thumbnail"><img :src="c.image"></div>
+        <h6>{{ c.name }}</h6>
       </div>
     </div>
 
     <div class="pagination">
       <ol>
-        <li><a>1</a></li>
+        <li class="active"><a>1</a></li>
         <li><a>2</a></li>
         <li><a>3</a></li>
         <li><a>4</a></li>
@@ -37,6 +37,7 @@ export default {
       pageTitle: '社區實踐',
       pageTitleEn: 'Company Archievements',
       backgroundImage: '/assets/images/company_archievements_banner.webp',
+      cases: []
     }
   },
   components: {},
@@ -61,7 +62,184 @@ export default {
   async asyncData({ params }) {
     const region = params.region
     return { region }
+  },
+  async fetch() {
+    //const response = await fetch(process.env.API_URL + 'api/slides')
+    //const data = await response.json()
+
+    this.cases = [
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+      {
+        id: 1,
+        image: "/assets/images/case.webp",
+        name: "七月沐樂",
+        region: "北部"
+      },
+    ]
   }
 }
 </script>
 
+<style lang="scss" scoped>
+.page-content {
+  padding-top: 90px;
+
+  @include small-screen {
+    padding-top: 60px;
+  }
+}
+.form-tab {
+  margin-bottom: 50px;
+}
+.case-wrap {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-content: space-around;
+  row-gap: 80px;
+
+  .case {
+    .thumbnail {
+      width: 335px;
+      height: 250px;
+
+      img {
+        width: 100%;
+        height: 100%;
+      }
+      @include small-screen {
+        width: 175px;
+        height: 133px;
+      }
+    }
+    h6 {
+      font-size: 20px;
+      margin-top: 20px;
+
+      @include small-screen {
+        font-size: 16px;
+        margin-top: 12px;
+      }
+    }
+  }
+
+  @include small-screen {
+    grid-template-columns: 1fr 1fr;
+    row-gap: 40px;
+  }
+}
+.pagination {
+  margin: 0 auto;
+  margin-top: 100px;
+
+  @include small-screen {
+    margin-top: 40px;
+  }
+  ol {
+    list-style-type: none;
+    display: flex;
+    column-gap: 8px;
+
+    li {
+
+      width: 32px;
+      height: 32px;
+      line-height: 28px;
+      text-align: center;
+      vertical-align: middle;
+      border: 1px #fff solid;
+
+      a {
+        font-size: 16px;
+      }
+    }
+    .active {
+      color: #333;
+      background-color: $main-color;
+      border-color: $main-color;
+    }
+  }
+}
+</style>
