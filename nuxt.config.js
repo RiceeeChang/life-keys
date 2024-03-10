e0eaef9e78f19071a0a8e716c1e32fd0f0f3aeee
Rice <riceeechang@gmail.com>
2024-03-10 16:36:49 +0800

    remove lottieCode

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    "~/plugins/lottie-vue-player.client.js"
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
